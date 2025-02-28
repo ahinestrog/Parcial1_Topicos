d5be39217ecad6e6a9f20632406e9d002e97fcd8
Alejandro Hinestroza <ahinestrog@eafit.edu.co>
2025-02-28 08:17:06 -0500

    parcial1

--- flights/urls.py
+++ flights/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, FlightRegistrationView
+from .views import HomePageView, FlightRegistrationView, FlightListView, FlightStatisticsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('flight_registration/', FlightRegistrationView.as_view(), name='flight_registration'),
+    path('flight_list/', FlightListView.as_view(), name='flight_list'),
+    path('flight_statistics/', FlightStatisticsView.as_view(), name='flight_statistics'),
     ]
