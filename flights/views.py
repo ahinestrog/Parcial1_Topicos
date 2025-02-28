3d4362fdf41bf095ea402cc541c16efe504440b9
Alejandro Hinestroza <ahinestrog@eafit.edu.co>
2025-02-28 08:15:02 -0500

    Parcial1

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from flights.forms import FlightForm
+from flights.models import Flight
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -13,4 +14,22 @@ class FlightRegistrationView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['flight_form'] = FlightForm()
+        return context
+    
+# Listar vuelos
+
+class FlightListView(TemplateView):
+    template_name = 'flight_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['flights'] = Flight.objects.all()
+        return context
+
+# Estadisticas de vuelos
+class FlightStatisticsView(TemplateView):
+    template_name = 'flight_statistics.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['flights'] = Flight.objects.all()
         return context
